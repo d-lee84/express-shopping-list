ef0c1441295eaa446fa93fa5d9a28d394e8d6ba7
David Lee <davidlee980804@gmail.com>
2021-01-06 17:12:21 -0800

    Updated the routing using json and middleware

--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -6,13 +6,22 @@ const router = express.Router();
 
 const HTTP_CREATED = 201;
 
+let list;
+
+/** Middleware to define the list from the list.json */
+
+router.use(async function(req, res, next) {
+  // Other ways to do this
+  // req.list = await readListFromFile();
+  // res.locals.list = await readListFromFile();
+  list = await readListFromFile();
+  return next();
+});
+
 /** Give a list of shopping items */
 
 router.get("/", async function(req, res, next) {
-  let items = await readListFromFile();
-  
-  return res.json(items);
-  // return res.json({items: db.items});
+  return res.json(list);
 });
 
 
@@ -22,11 +31,9 @@ router.post("/", async function(req, res, next) {
   // Object Destructing
   let {name, price} = req.body;
 
-  let list = await readListFromFile();
   list.items.push({name, price});
-  await writeToFile(list);
 
-  // db.items.push({name, price});
+  await writeToFile(list);
 
   return res.status(HTTP_CREATED).json({added: {name, price}});
 });
@@ -34,26 +41,33 @@ router.post("/", async function(req, res, next) {
 /** Get a single item from the shopping list */
 
 router.get("/:name", function(req, res, next) {
-  let item = db.items.find(item => item.name === req.params.name);
+
+  let item = list.items.find(item => item.name === req.params.name);
 
   return res.json(item);
 });
 
 /** Modifies a single item from the shopping list */
 
-router.patch("/:name", function(req, res, next) {
-  let item = db.items.find(item => item.name === req.params.name);
+router.patch("/:name", async function(req, res, next) {
+
+  let item = list.items.find(item => item.name === req.params.name);
 
   item.name = req.body.name;
   item.price = req.body.price;
+
+  await writeToFile(list);
 
   return res.json({updated: item});
 });
 
 /** Deletes a single item from the shopping list */
 
-router.delete("/:name", function(req, res, next) {
-  db.items = db.items.filter(item => item.name !== req.params.name);
+router.delete("/:name", async function(req, res, next) {
+
+  list.items = list.items.filter(item => item.name !== req.params.name);
+
+  await writeToFile(list);
 
   return res.json({message: "Deleted"});
 });
